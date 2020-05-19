fa06452174b4581f3fa64936d7f6dbfc94bd8134
bslzz <bishalkoid@gmail.com>
2020-05-19 16:30:47 +0300

    testing functionality

--- myfrontend/src/Components/IndexWheel/IndexWheel.js
+++ myfrontend/src/Components/IndexWheel/IndexWheel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wheel from '../wheel/Appwheel';
-import './Indexwheel.css';
+import './IndexWheel.css';
 
 export const QuizCategories = [
   { id: 9, name: 'General knowledge' },
